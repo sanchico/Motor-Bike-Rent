9f768c0fccf19e67bc357b5bfd4622a13ddce73a
Jose <joseda30180@gmail.com>
2018-07-12 16:59:52 +0200

    Version original

---
+++ angularjs/src/main/java/com/proyecto/angularjs/models/User.java
@@ -0,0 +1,91 @@
+package com.proyecto.angularjs.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString(exclude = "password")
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 9052727407992747243L;
+
+	public static final PasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder();
+
+	private @Id @GeneratedValue Long id;
+
+    private String name;
+    
+    private String role;
+
+    private @JsonIgnore String password;
+
+	protected User() {
+    }
+
+	public User(String name, String password, String role) {
+        this.name = name;
+        this.setPassword(password);
+        this.role = role;
+    }
+    
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+	public void setPassword(String password) {
+		this.password = PASSWORD_ENCODER.encode(password);
+	}
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @return the role
+     */
+    public String getRole() {
+        return role;
+    }
+
+    /**
+     * @param role the role to set
+     */
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
